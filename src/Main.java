a84c2b2d7f90f5b3afc370855af88ea0515ee916
iyuna <dbsk2778@unidcompany.com>
2024-03-11 21:21:19 +0900

    #feature/dynamic_programming add: 피보나치 수열 예시 추가

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import DP.Fibonacci;
+import java.util.Scanner;
+
 public class Main
 {
     
-    public static void main (String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("숫자를 입력하세요 : ");
+        
+        int n = scanner.nextInt();
+        
+        Fibonacci fibonacci = new Fibonacci();
+        int result = fibonacci.fib(n);
+        
+        System.out.println("결과는 " + result + "입니다.");
+        
+        scanner.close();
     }
 }
