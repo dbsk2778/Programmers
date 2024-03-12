cda7e2f28cf09c64702eab5415902fe887b6719e
iyuna <dbsk2778@unidcompany.com>
2024-03-12 23:08:41 +0900

    #feature/arrays add: toCharArray() 함수 예시 추가

--- src/str_algorithm/StrArrays.java
+++ src/str_algorithm/StrArrays.java
@@ -12,6 +12,12 @@ public class StrArrays
         strArray1(str);
         strArray2(str);
         stringBuilderTest(str);
+    
+        String myString = "cvsgiorszzzmrpaqpe";
+        int[] indexList = {16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7};
+        
+        System.out.println(charListTest(myString, indexList));
+        System.out.println(charListTest2(myString, indexList));
     }
     
     public static void strArray1 (String str) {
@@ -37,10 +43,35 @@ public class StrArrays
         System.out.println(chars.length); // str의 길이는 length로 구할 수 있음
     }
     
+    // my_string의 index_list의 원소들에 해당하는 인덱스의 글자들을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+    public static String charListTest (String myString, int[] indexList) {
+        String answer = "";
+        char[] charList = myString.toCharArray();
+    
+        for (int i = 0; i < indexList.length; i++) {
+            answer += charList[indexList[i]];
+        }
+        return answer;
+    }
+    
+    public static String charListTest2 (String myString, int[] indexList) {
+        StringBuilder answer = new StringBuilder();
+        char[] charList = myString.toCharArray();
+    
+        for ( int i : indexList )
+            {
+                answer.append(charList[i]);
+            }
+        return answer.toString();
+    }
+    
+    
     public static void stringBuilderTest(String str) {
         StringBuilder sb = new StringBuilder(str);
         String reverseString = sb.reverse().toString();
         
         System.out.println(reverseString);
     }
+    
+    
 }
