ea7ef07d2fd6c33ee0af651c5b114aba6049aa0c
iyuna <dbsk2778@unidcompany.com>
2024-05-06 19:38:00 +0900

    programmers_basic add: programmers 181914 문제 풀이 추가 (BigInteger)

---
+++ src/basics/day_4/programmers_181914.java
@@ -0,0 +1,17 @@
+package basics.day_4;
+
+import java.math.BigInteger;
+
+public class programmers_181914 {
+  // 181914 - 9로 나눈 나머지 구하기
+  public static void main(String[] args) {
+    System.out.println(solution("123456789"));
+    System.out.println(solution("78720646226947352489"));
+  }
+
+  // long을 넘어서는 큰 수를 다룰 때 BigInteger 사용
+  public static int solution(String number) {
+    BigInteger num = new BigInteger(number);
+    return num.mod(BigInteger.valueOf(9)).intValue();
+  }
+}
