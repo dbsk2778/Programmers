f0892e9baff418769620d6be52abf11b2ddaee2a
iyuna <dbsk2778@unidcompany.com>
2024-05-25 08:15:06 +0900

    programmers_basic add: programmers 181888 가까운 1찾기 문제 추가

---
+++ src/basics/day_5/programmers_181898.java
@@ -0,0 +1,24 @@
+package basics.day_5;
+
+public class programmers_181898 {
+
+  /*
+    프로그래머스 181898 가까운 1찾기
+   */
+  public static void main(String[] args) {
+    System.out.println(solution(new int[]{0, 0, 0, 1},1));
+  }
+
+  public static int solution(int[] arr, int idx) {
+    int answer = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (i >= idx && arr[i] == 1) {
+        answer = i;
+        break;
+      } else {
+        answer = -1;
+      }
+    }
+    return answer;
+  }
+}
