9f6da2262a0665b24cb01776aa25191ebbd604e8
iyuna <dbsk2778@unidcompany.com>
2024-06-06 10:25:27 +0900

    programmers_basic add: programmers 181829, 181860 코드 추가

--- src/basics/day_5/programmers_181838.java
+++ src/basics/day_5/programmers_181838.java
@@ -1,6 +1,7 @@
 package basics.day_5;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class programmers_181838 {
 
@@ -12,10 +13,8 @@ public class programmers_181838 {
     int[] date1 = {2021, 12, 28};
     int[] date2 = {2021, 12, 29};
     System.out.println(solution(date1, date2));
-
-    int[] date3 = {2021, 12, 28};
-    int[] date4 = {2021, 12, 29};
-    System.out.println(solution2(date3, date4));
+    System.out.println(solution2(date1, date2));
+    System.out.println(solution3(date1, date2));
   }
 
   public static int solution(int[] date1, int[] date2) {
@@ -36,5 +35,10 @@ public class programmers_181838 {
     LocalDate new_date2 = LocalDate.of(date2[0], date2[1], date2[2]);
     // LocalDate 형식에는 isBefore(), isAfter(), isEqual() 메서드가 있음 !!
     return new_date1.isBefore(new_date2) ? 1 : 0;
+  }
+
+  public static int solution3(int[] date1, int[] date2) {
+    // 배열 값 비교 : date1이 date2보다 이르면 음수를 반환
+    return Arrays.compare(date1, date2) < 0 ? 1 : 0;
   }
 }
