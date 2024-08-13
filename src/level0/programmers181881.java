2f2c4663758f8981476501126b8235c158993d90
dbsk2778 <dbsk2778@gmail.com>
2024-08-13 22:10:17 +0900

    프로그래머스 level0 연습문제 - 왼쪽 오른쪽 다른 방식 추가

--- src/level0/programmers181881.java
+++ src/level0/programmers181881.java
@@ -1,9 +1,11 @@
 package level0;
 
+import java.util.Arrays;
+
 public class programmers181881 {
 
     public static void main(String[] args) {
-        System.out.println(solution(new int[] {1, 2, 3, 100, 99, 98}));
+        System.out.println(solution(new int[]{1, 2, 3, 100, 99, 98}));
     }
 
     public static int solution(int[] arr) {
@@ -47,5 +49,29 @@ public class programmers181881 {
         }
 
         return answer - 1;
+    }
+
+    public static int solution2(int[] arr) {
+        int count = 0;
+        while (true) {
+            int[] copied = Arrays.copyOf(arr, arr.length);
+
+            for (int i = 0; i < copied.length; i++) {
+                if (copied[i] >= 50 && copied[i] % 2 == 0) {
+                    copied[i] /= 2;
+                } else if (copied[i] < 50 && copied[i] % 2 == 1) {
+                    copied[i] = copied[i] * 2 + 1;
+                }
+            }
+
+            // 배열이 동일한지 확인
+            if (Arrays.equals(arr, copied)) {
+                return count;
+            }
+
+            // 배열을 이렇게 바로 대입 가능
+            arr = copied;
+            count++;
+        }
     }
 }
